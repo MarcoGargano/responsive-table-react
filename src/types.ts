05c7a23cfece5f6bb6bdacbcfd4f3645d423464b
Marco Gargano <mail@marcogargano.com>
2023-03-18 18:06:48 +0100

    Support key type with string and number

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
 
-export type ResponsiveTableColumn = { id: string, text: string }
+export type ResponsiveTableColumn = { id: string | number, text: string }
 
 export type TableProps = {
-  data: Record<string, ReactNode>[];
+  data: Record<string | number, ReactNode>[];
   columns: ResponsiveTableColumn[]
 };
